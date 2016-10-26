408830a2fbb7532c21b59ffae80eef93929638f4
Mario T <marioterron157@gmail.com>
2016-10-27 00:44:42 +0200

    Fixed bugs

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,5 @@
 $(function() {
-  $('.header, .header--sm, .contact, .location').prepend('<div class="overlay">');
+  $('.header, .header--sm, .contact, .location, .charities').prepend('<div class="overlay">');
   $('#nav-open').click(function() {$('html').addClass('active');});
   $('#nav-close').click(function() {$('html').removeClass('active');});
   $('.nav__item').click(function() {$('html').removeClass('active');});
--- js/min/menu-min.js
+++
@@ -1,1 +0,0 @@
-$(function(){$(".header, .header--sm, .contact, .location").prepend('<div class="overlay">'),$("#nav-open").click(function(){$("html").addClass("active")}),$("#nav-close").click(function(){$("html").removeClass("active")}),$(".nav__item").click(function(){$("html").removeClass("active")})});
