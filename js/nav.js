042565224b65aff7113a63322dcf2bc795ce1e34
Mario T <marioterron157@gmail.com>
2016-11-25 18:43:13 +0100

    Fixed some bugs

--- js/nav.js
+++ js/nav.js
@@ -1,9 +1,7 @@
 $(function() {
-	$('.header, .header--sm').prepend('<div class="overlay">');
-    $('.contact').prepend('<div class="overlay">');
-	$('.location').prepend('<div class="overlay">');
-	$('.charities').prepend('<div class="overlay">');
+	$('.header, .header--sm, .charities, .location, .contact').prepend('<div class="overlay">');
   	$('#nav-open').click(function() {$('html').addClass('active');});
   	$('#nav-close').click(function() {$('html').removeClass('active');});
   	$('.nav__item').click(function() {$('html').removeClass('active');});
+	$('.overlay').click(function() {$('html').removeClass('active');});
 });
